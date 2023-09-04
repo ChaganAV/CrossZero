4948d12d0b946d37ab5504409404a0c3a797130a
Alexandr <jwchaganav@gmail.com>
2023-09-04 17:00:48 +0400

    Обработка победы

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -25,19 +25,30 @@ public class Program {
      * @param args вожможные аргументы программы
      */
     public static void main(String[] args) {
-        initialize();
+        int X = 3;
+        int Y = 3;
+        initialize(X,Y);
         printField();
-        turnHuman();
-        turnAI();
-
+        while (true){
+            turnHuman();
+            printField();
+            if (checkGameState(DOT_NUMAN,String.format("Игра закончена, выиграли %s", DOT_NUMAN))) {
+                break;
+            }
+            turnAI();
+            printField();
+            if (checkGameState(DOT_AI,String.format("Игра закончена, выиграли %s", DOT_AI))) {
+                break;
+            }
+        }
     }
 
     /**
      * Инициализация игрового поля
      */
-    private static void initialize(){
-        fieldSizeX = 5;
-        fieldSizeY = 5;
+    private static void initialize(int X, int Y){
+        fieldSizeX = X;
+        fieldSizeY = Y;
         field = new char[fieldSizeX][fieldSizeY];
         for(int x = 0; x < fieldSizeX; x++){
             for (int y = 0; y < fieldSizeY; y++) {
@@ -82,16 +93,36 @@ public class Program {
     /**
      * Обработка хода человека
      */
-    private static void turnHuman(){
+    private static void turnHuman() {
         int x = 0, y = 0;
-        do{
-            System.out.println(String.format("Введите координаты хода X и Y (от %d до %d)",fieldSizeX,fieldSizeY));
-            x = scanner.nextInt() - 1;
-            y = scanner.nextInt() - 1;
-        }while (!isCellValid(x,y) || !isCellEmpty(x,y));
+        do {
+            while (true) {
+                System.out.println(String.format("Введите координаты хода X (от %d до %d)", fieldSizeX));
+                if (scanner.hasNextInt()) {
+                    x = scanner.nextInt() - 1;
+                    scanner.nextLine();
+                    break;
+                } else {
+                    System.out.println("Некорректное число, повторите попытку ввода");
+                    scanner.nextLine();
+                }
+            }
+
+            while(true){
+                System.out.println(String.format("Введите координаты хода Y (от %d до %d)", fieldSizeY));
+                if (scanner.hasNextInt()) {
+                    y = scanner.nextInt() - 1;
+                    scanner.nextLine();
+                    break;
+                } else {
+                    System.out.println("Некорректное число, повторите попытку ввода");
+                    scanner.nextLine();
+                }
+            }
+
+        }while (!isCellValid(x, y) || !isCellEmpty(x, y)) ;
         field[x][y] = DOT_NUMAN;
     }
-
     /**
      * Обработка хода компьютера
      */
@@ -126,18 +157,36 @@ public class Program {
     }
 
     /**
-     * Проверки по горизонтали, вертикали, диагоналям
+     * Проверка состояния игры
+     * @param c фишка игрока
+     * @param message сообщение
+     * @return
+     */
+    private static boolean checkGameState(char c, String message){
+        if(checkWin(c)){
+            System.out.println(message);
+            return true;
+        }
+        if(checkDraw()){
+            System.out.println("Ничья");
+            return true;
+        }
+        return false; // Игра продолжается
+    }
+    /**
+     * Проверки Победы - по горизонтали, вертикали, диагоналям
      * @param c
      * @return
      */
     private static boolean checkWin(char c){
-        // время 1.13
-        // Простой вариант проверки по горизонтали
+        //
+        /* Простой вариант проверки по горизонтали
         if (field[0][0] == c && field[0][1] == c && field[0][2] == c && field[0][3] == c && field[0][4] == c) return true;
         if (field[1][0] == c && field[1][1] == c && field[1][2] == c && field[1][3] == c && field[1][4] == c) return true;
         if (field[2][0] == c && field[2][1] == c && field[2][2] == c && field[2][3] == c && field[2][4] == c) return true;
         if (field[3][0] == c && field[3][1] == c && field[3][2] == c && field[3][3] == c && field[3][4] == c) return true;
         if (field[4][0] == c && field[4][1] == c && field[4][2] == c && field[4][3] == c && field[4][4] == c) return true;
+         */
 
         int count = 0;
         // Проверки по горизонтали
@@ -145,21 +194,60 @@ public class Program {
             for (int y = 0; y < fieldSizeY; y++) {
                 if(field[x][y] == c) count++;
             }
+            if(count==fieldSizeX) return true;
+            count = 0;
         }
         // Проверки по вертикали
-        // Проверки по диагонали X
-        for (int x = 0; x < fieldSizeX; x++) {
-            if (field[x][x] == c) count++;
+        count = 0;
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if(field[x][y] == c) count++;
+            }
+            if(count==fieldSizeY) return true;
+            count = 0;
         }
-        if (count == fieldSizeX) return true;
-
+        // Проверки по диагонали X
+        count = 0;
+        for (int x = 0; x < fieldSizeX; x++) {
+            for (int y = 0; y < fieldSizeY; y++) {
+                if (x == y){
+                    if(field[x][y] == c) count++;
+                }
+            }
+        }
+        if (fieldSizeX >= fieldSizeY){
+            if (count == fieldSizeX) return true;
+        }else {
+            if (count == fieldSizeY) return true;
+        }
         // Проверка по диагонали Y
         count = 0;
-        for (int y = 0; y < fieldSizeX; y++) {
-            if (field[y][y] == c) count++;
+        for (int x = fieldSizeX-1; x >=0; x--) {
+            for (int y = fieldSizeY; y >= 0; y--) {
+                if (x == y){
+                    if(field[x][y] == c) count++;
+                }
+            }
         }
-        if (count == fieldSizeY) return true;
+        if (fieldSizeX >= fieldSizeY){
+            if (count == fieldSizeX) return true;
+        }else {
+            if (count == fieldSizeY) return true;
+        }
 
         return false;
+    }
+
+    /**
+     * Проверка на ничью
+     * @return метод вернет истину, если ничья
+     */
+    private static boolean checkDraw(){
+        for (int x = 0; x < fieldSizeX; x++) {
+            for (int y = 0; y < fieldSizeY; y++) {
+                if(isCellEmpty(x,y)) return false;
+            }
+        }
+        return true;
     }
 }
