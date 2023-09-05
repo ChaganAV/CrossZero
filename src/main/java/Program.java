1d50f0fdf493a52eb86425c729cbf32f06b15fc6
Alexandr <jwchaganav@gmail.com>
2023-09-05 12:30:07 +0400

    изменил проверку победы

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -16,28 +16,39 @@ public class Program {
     private static final Random random = new Random();
 
     private static char[][] field; // Двумерный массив, хранит текущее состояние игрового поля
+    private static int[] point = new int[2]; // Выбранные
 
     private static int fieldSizeX; // Размерность игрового поля
     private static int fieldSizeY; // Размерность игрового поля
+
+    private static int minVictory;
+    private static int areaSize;
 
     /**
      * Точка входа в программу
      * @param args вожможные аргументы программы
      */
     public static void main(String[] args) {
-        int X = 3;
-        int Y = 3;
+        areaSize = 5;
+        int X = 5;
+        int Y = 5;
+        if (areaSize>3){
+            minVictory = 4;
+        }else{
+            minVictory = areaSize;
+        }
+
         initialize(X,Y);
         printField();
         while (true){
             turnHuman();
             printField();
-            if (checkGameState(DOT_NUMAN,String.format("Игра закончена, выиграли %s", DOT_NUMAN))) {
+            if (checkGameState(DOT_NUMAN)) {
                 break;
             }
             turnAI();
             printField();
-            if (checkGameState(DOT_AI,String.format("Игра закончена, выиграли %s", DOT_AI))) {
+            if (checkGameState(DOT_AI)) {
                 break;
             }
         }
@@ -97,7 +108,7 @@ public class Program {
         int x = 0, y = 0;
         do {
             while (true) {
-                System.out.println(String.format("Введите координаты хода X (от %d до %d)", fieldSizeX));
+                System.out.println(String.format("Введите координаты хода X (от 0 до %d)", fieldSizeX));
                 if (scanner.hasNextInt()) {
                     x = scanner.nextInt() - 1;
                     scanner.nextLine();
@@ -109,7 +120,7 @@ public class Program {
             }
 
             while(true){
-                System.out.println(String.format("Введите координаты хода Y (от %d до %d)", fieldSizeY));
+                System.out.println(String.format("Введите координаты хода Y (от 0 до %d)", fieldSizeY));
                 if (scanner.hasNextInt()) {
                     y = scanner.nextInt() - 1;
                     scanner.nextLine();
@@ -121,6 +132,8 @@ public class Program {
             }
 
         }while (!isCellValid(x, y) || !isCellEmpty(x, y)) ;
+        point[0] = x;
+        point[1] = y;
         field[x][y] = DOT_NUMAN;
     }
     /**
@@ -159,12 +172,11 @@ public class Program {
     /**
      * Проверка состояния игры
      * @param c фишка игрока
-     * @param message сообщение
      * @return
      */
-    private static boolean checkGameState(char c, String message){
+    private static boolean checkGameState(char c){
         if(checkWin(c)){
-            System.out.println(message);
+            System.out.println(String.format("Игра закончена, выиграли %s",c));
             return true;
         }
         if(checkDraw()){
@@ -175,10 +187,12 @@ public class Program {
     }
     /**
      * Проверки Победы - по горизонтали, вертикали, диагоналям
-     * @param c
+     * @param c // фишка игрока
      * @return
      */
     private static boolean checkWin(char c){
+        int X = point[0];
+        int Y = point[1];
         //
         /* Простой вариант проверки по горизонтали
         if (field[0][0] == c && field[0][1] == c && field[0][2] == c && field[0][3] == c && field[0][4] == c) return true;
@@ -188,52 +202,127 @@ public class Program {
         if (field[4][0] == c && field[4][1] == c && field[4][2] == c && field[4][3] == c && field[4][4] == c) return true;
          */
 
-        int count = 0;
-        // Проверки по горизонтали
-        for (int x = 0; x < fieldSizeX; x++) {
-            for (int y = 0; y < fieldSizeY; y++) {
-                if(field[x][y] == c) count++;
+
+        // Проверки по вертикали
+        /*int count = 0;
+        for (int x = X; x < fieldSizeX; x++) {
+            for (int y = Y; y < fieldSizeY; y++) {
+                if(field[x][y] == c){
+                    count++;
+                }else {
+                    break;
+                }
             }
             if(count==fieldSizeX) return true;
             count = 0;
+        }*/
+        // Проверка по вертикали в обе стороны
+        int count = 0;
+        for (int x = X; x < fieldSizeX; x++) {
+            if(field[x][Y] == c){
+                count++;
+            }else{
+                break;
+            }
         }
-        // Проверки по вертикали
+        for (int x = X-1; x >= 0; x--) {
+            if(field[x][Y] == c){
+                count++;
+            }else {
+                break;
+            }
+        }
+        if(count>=minVictory) return true;
+
+        // Проверки по горизонтали
         count = 0;
+        /*
         for (int y = 0; y < fieldSizeY; y++) {
             for (int x = 0; x < fieldSizeX; x++) {
                 if(field[x][y] == c) count++;
             }
             if(count==fieldSizeY) return true;
             count = 0;
+        }*/
+        for (int y = Y; y < fieldSizeY; y++) {
+            if(field[X][y] == c){
+                count++;
+            }else{
+                break;
+            }
         }
-        // Проверки по диагонали X
+        for (int y = Y-1; y >= 0; y--) {
+            if(field[X][y] == c){
+                count++;
+            }else {
+                break;
+            }
+        }
+        if(count>=minVictory) return true;
+
+        // Проверки диагонали по X
         count = 0;
+        /*
         for (int x = 0; x < fieldSizeX; x++) {
             for (int y = 0; y < fieldSizeY; y++) {
                 if (x == y){
                     if(field[x][y] == c) count++;
                 }
             }
-        }
-        if (fieldSizeX >= fieldSizeY){
-            if (count == fieldSizeX) return true;
-        }else {
-            if (count == fieldSizeY) return true;
-        }
-        // Проверка по диагонали Y
-        count = 0;
-        for (int x = fieldSizeX-1; x >=0; x--) {
-            for (int y = fieldSizeY; y >= 0; y--) {
-                if (x == y){
-                    if(field[x][y] == c) count++;
+        }*/
+        int offset = X-Y; // шаг смещения
+        for (int x = X; x < fieldSizeX; x++) {
+            for (int y = Y; y < fieldSizeY; y++) {
+                if(X-Y-offset == 0) {
+                    if (field[x][y] == c) {
+                        count++;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            for (int y = Y-1; y >= 0 ; y--) {
+                if(X-Y-offset == 0) {
+                    if (field[x][y] == c) {
+                        count++;
+                    } else {
+                        break;
+                    }
                 }
             }
         }
-        if (fieldSizeX >= fieldSizeY){
-            if (count == fieldSizeX) return true;
-        }else {
-            if (count == fieldSizeY) return true;
+        if (count >= minVictory) return true;
+
+        // Проверка диагонали по Y
+        count = 0;
+//        for (int x = fieldSizeX-1; x >=0; x--) {
+//            for (int y = fieldSizeY; y >= 0; y--) {
+//                if (x == y){
+//                    if(field[x][y] == c) count++;
+//                }
+//            }
+//        }
+        for (int y = Y; y < fieldSizeY; y++) {
+            for (int x = X; x < fieldSizeX; x++) {
+                if(X-Y-offset == 0) {
+                    if (field[x][y] == c) {
+                        count++;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            for (int x = X-1; x >= 0 ; x--) {
+                if(X-Y-offset == 0) {
+                    if (field[x][y] == c) {
+                        count++;
+                    } else {
+                        break;
+                    }
+                }
+            }
         }
+        if (count == fieldSizeX) return true;
 
         return false;
     }
